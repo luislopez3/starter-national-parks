c001d90935b03dca20bf61cda3acf4f597b172a7
Luis Lopez <llopez1518@gmail.com>
2021-05-04 19:45:53 -0600

    Updating the DOM Checkpoint

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+/*
+**************** Selecting Elements on the DOM Checkpoint *****************
+
 // Using "querySelector()"
 console.log(document.querySelector(".value"));
 console.log(document.querySelector("button"));
@@ -33,4 +36,59 @@ for (let i = 0; i < allAreas.length; i++) {
     const element = allAreas[i];
     console.log(element);
 }
+*/
+
+/********************* Updating the DOM Checkpoint *****************/
+
+// Changing the content using the "innerText" property
+const descriptions = document.querySelectorAll(".description-display");
+
+for (let desc of descriptions.values()) {
+    let content = desc.innerText;
+    console.log(content);
+
+    if (content.length > 250) {
+        content = content.slice(0, 250);
+        content = content + '<a href="#">...</a>';
+    }
+    desc.innerHTML = content;
+}
+
+const ratings = document.querySelectorAll(".rating-display .value");
+
+for (let rating of ratings) {
+    let ratingValue = parseFloat(rating.innerText);
+
+    if (ratingValue > 4.7) {
+        rating.style.fontWeight = "bold";
+        rating.style.color = "#3ba17c"
+    }
+    console.log(ratingValue);
+}
+
+// Creating DOM Elements using the "createElement()" & "appendChild()" methods
+// First, select the parks on the page using the "park" class as a selector:
+const parks = document.querySelectorAll(".park-display");
+// Then, get the number of parks using the "length" property of the list:
+const numberParks = parks.length;
+// Next, create a new element. In this case, you should use a <div>:
+const newElement = document.createElement("div");
+// You can set the text of this element with the "innerText" property, like this:
+newElement.innerText = `${numberParks} exciting parks to visit`;
+// Using "classList" to add the new "header-statement" class added to the CSS file:
+// Then add this class to the new element:
+newElement.classList.add("header-statement");
+// Finally, to add the new element to the DOM, select the <header> element and use
+// the "appendChild()" method to add the new element to the <header>, as shown below:
+const header = document.querySelector("header");
+header.appendChild(newElement);
+
+// Removing elements from the DOM
+// For example, you can select the first park on the park and remove it:
+// Get the parent element of all parks
+const main = document.querySelector("main");
+// Select a single park
+const park = main.querySelector(".park-display");
+// Remove that park
+//main.removeChild(park);
 
